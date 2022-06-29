387c6ecbbf102cd1f6f76725c8a3c52afdbb353f
Vion Nguyen <hiepnm@vmodev.com>
2022-06-29 09:15:42 +0700

    add page post1 and signin

---
+++ pages/list-post/post1.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import React from "react";
+
+export default function Post1(){
+    return(
+        <div>
+            Post 1
+        </div>
+    )
+}
---
+++ pages/signin.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import React from 'react'
+
+const Signin: NextPage = () => {
+    return(
+        <div>
+            Sign In Page
+        </div>
+    )
+}
+
+export default Signin
